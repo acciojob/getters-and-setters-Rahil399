e692a7b15d4a6f52a3d57a74417a7a2f33d3eea2
Rahil Abdul <153207425+Rahil399@users.noreply.github.com>
2025-01-15 23:56:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-//complete this code
-
 class Person {
-	constructor(name, age){
-	this.name = name;
-	this.age = age;
-	}
+  constructor(name, age) {
+    this._name = name; // Use underscore-prefixed properties
+    this._age = age;
+  }
+
+  get name() {
+    return this._name; // Getter for name
+  }
+
+  set age(newAge) {
+    this._age = newAge; // Setter for age
+  }
 }
+
 class Student extends Person {
-	study() {
-		console.log(`${this.name} is studying.`);
-	}
+  study() {
+    console.log(`${this.name} is studying.`);
+  }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(`${this.name} is studying.`);
-	}
+  teach() {
+    console.log(`${this.name} is teaching.`);
+  }
 }
 
 // Do not change the code below this line
